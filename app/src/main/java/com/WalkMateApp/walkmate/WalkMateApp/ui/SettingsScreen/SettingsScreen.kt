b7e70b066c9345e6454de68af0207daaa71031d1
hP <balochkhubaib008@gmail.com>
2024-05-31 18:46:17 +0500

    worked on home screen.

--- app/src/main/java/com/WalkMateApp/walkmate/WalkMateApp/ui/SettingsScreen/SettingsScreen.kt
+++ app/src/main/java/com/WalkMateApp/walkmate/WalkMateApp/ui/SettingsScreen/SettingsScreen.kt
@@ -1,10 +1,29 @@
 package com.WalkMateApp.walkmate.WalkMateApp.ui.SettingsScreen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.WalkMateApp.walkmate.R
+import com.WalkMateApp.walkmate.ui.theme.MidnightBlue
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SettingsScreen(navController: NavController)
-{
-
+fun SettingsScreen(navController: NavController) {
 }
